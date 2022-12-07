a127ef68f8a4d41c68d938cb91841036e97c4cb7
florenciaCardinali <you@example.com>
2022-12-07 15:10:25 -0600

    cambio nombre inicio a plantilla, avance en app

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
-
-#variable app es un objeto nuevo de tipo flask
+from flask import Flask, render_template
+import json
+#instancimiento de flas
+    #variable app es un objeto nuevo de tipo flask
 app = Flask(__name__)
- 
-#desde aca empiesan los enrutamientos
+
+#tratamiento de json
+def obtener_usuarios():
+    with open("users.json") as data:
+        datos_user = json.load(data)
+    return datos_user
+
+#enrrutamientos de la app
+    #desde aca empiesan los enrutamientos
 @app.route("/")
 def index():
-    return "Bienvenidos al servicio de recomendacion de peliculas!"
+    return render_template("/plantilla.html")
+
 
 #si el nombre del objeto flask es el main, ejecutalo.
 if __name__ =="__main__":
     app.run(debug=True)
-     
+#no poner nada despues de esto, sino no se muestra.
