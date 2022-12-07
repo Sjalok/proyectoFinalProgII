fa582c757c89f4aae21e6fd19dfb73ca39d2f5b6
florenciaCardinali <you@example.com>
2022-12-07 12:55:31 -0600

    funcionando con flask y comentarios explicativos

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-import Flask
+from flask import Flask
+
+#variable app es un objeto nuevo de tipo flask
+app = Flask(__name__)
+ 
+#desde aca empiesan los enrutamientos
+@app.route("/")
+def index():
+    return "Bienvenidos al servicio de recomendacion de peliculas!"
+
+#si el nombre del objeto flask es el main, ejecutalo.
+if __name__ =="__main__":
+    app.run(debug=True)
+     
