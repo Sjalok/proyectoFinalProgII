3377be523ab2b8a13116f98e973bab4e6b70fe94
florenciaCardinali <you@example.com>
2022-12-07 19:29:35 -0600

    organisacion en carpeta temlates

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ import json
 app = Flask(__name__)
 
 #tratamiento de json
-def obtener_usuarios():
-    with open("users.json") as data:
-        datos_user = json.load(data)
-    return datos_user
+with open("users.json") as data1:
+    datos_user = json.load(data1)
+
+with open("data.json") as data2:
+    datos_peliculas = json.load(data2)
 
 #enrrutamientos de la app
     #desde aca empiesan los enrutamientos
 @app.route("/")
 def index():
-    return render_template("/plantilla.html")
+    return render_template("main.html", data=datos_peliculas)
 
+@app.route("/login")
+def login():
+    return render_template("/ngresar.html")
 
 #si el nombre del objeto flask es el main, ejecutalo.
 if __name__ =="__main__":
